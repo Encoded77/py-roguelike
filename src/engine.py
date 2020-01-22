55217c54df7b0ae9c9f93711707469f054c03c48
Thomas <duprethomasfr@gmail.com>
2020-01-22 16:11:46 +0100

    [+]Part 2 of libtcod tutorial [+]Created entity, tile & gamemap classes [+]Added render functions to render gamemap & entities

--- src/engine.py
+++ src/engine.py
@@ -1,22 +1,39 @@
 import tcod as libtcod
 
 # imports
+from lib.entity_objects.entity import Entity
+from lib.map_objects.game_map import GameMap
+
 from input_handlers import handle_keys
+from render_functions import render_all, clear_all
 
 def main():
     # Set screen size
     screen_width = 80
     screen_height = 50
 
-    # Player infos
-    player_x = int(screen_width/2)
-    player_y = int(screen_height/2)
+    # Tilemap size
+    map_width = 80
+    map_height = 45
+
+    # Map colors
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    # Init some entities
+    player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
+    entities = [npc, player]
 
     # Assign custom tileset/fonts & init console
     libtcod.console_set_custom_font('./src/assets/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'my-py-roguelike', False)
 
     con = libtcod.console_new(screen_width, screen_height)
+
+    game_map = GameMap(map_width, map_height)
 
     # Mouse & keys infos
     key = libtcod.Key()
@@ -26,13 +43,9 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
-
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
         libtcod.console_flush() # Update window to display current state
-
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
 
         # Get action from key presses
         action = handle_keys(key)
@@ -43,8 +56,8 @@ def main():
 
         if move:
             dx, dy = move
-            player_x += dx
-            player_y += dy
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
 
         if exit:
             return True
