b7151d4e109f35f244b1edfd4b1518dbfce3167f
Thomas <duprethomasfr@gmail.com>
2020-01-23 12:29:04 +0100

    [+]Progress on part 7 [/]Health counter is now a health bar

--- src/engine.py
+++ src/engine.py
@@ -16,9 +16,13 @@ def main():
     screen_width = 80
     screen_height = 50
 
+    bar_width = 20
+    panel_height = 7
+    panel_y = screen_height - panel_height
+
     # Tilemap size
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     # Dungeon
     room_max_size = 10
@@ -48,7 +52,9 @@ def main():
     libtcod.console_set_custom_font('./src/assets/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'my-py-roguelike', False)
 
+    # Screen inits
     con = libtcod.console_new(screen_width, screen_height)
+    panel = libtcod.console_new(screen_width, panel_height)
 
     # init game map
     game_map = GameMap(map_width, map_height)
@@ -73,7 +79,8 @@ def main():
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
 
 
-        render_all(con, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, screen_width, screen_height,
+                   bar_width, panel_height, panel_y, colors)
         fov_recompute = False
 
         libtcod.console_flush() # Update window to display current state
