f5dc41ae7a7e21fb726652192e70308f8d998504
Thomas <duprethomasfr@gmail.com>
2020-01-28 16:00:25 +0100

    [/]Changed ring's color to be darker

--- src/lib/entity_objects/items/rings.py
+++ src/lib/entity_objects/items/rings.py
@@ -9,11 +9,11 @@ from lib.entity_objects.components.equippable import Equippable
 
 def iron_ring_force(x, y):
     equippable_component = Equippable(EquipmentSlots.FINGER, power_bonus=2)
-    return Entity(x, y, 'o', libtcod.gold, 'Iron ring of force', equippable=equippable_component,
+    return Entity(x, y, 'o', libtcod.darker_orange, 'Iron ring of force', equippable=equippable_component,
                   render_order=RenderOrder.ITEM)
 
 
 def iron_ring_life(x, y):
     equippable_component = Equippable(EquipmentSlots.FINGER, max_hp_bonus=15)
-    return Entity(x, y, 'o', libtcod.gold, 'Iron ring of life', equippable=equippable_component,
+    return Entity(x, y, 'o', libtcod.darker_orange, 'Iron ring of life', equippable=equippable_component,
                   render_order=RenderOrder.ITEM)
